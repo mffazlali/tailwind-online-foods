76566b2c0ad5b47d1e2dbe22ef91b8c14caf7002
mohammad fazlali <58655751+mffazlali@users.noreply.github.com>
2023-06-29 09:16:44 -0700

    complete online foods

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,8 @@
+const loader = document.querySelector('.loader')
+window.addEventListener('load', () => {
+    loader.classList.add('hidden')
+})
+
 const nav = document.getElementById('nav')
 const open = document.getElementById('open')
 const close = document.getElementById('close')
@@ -9,3 +14,33 @@ open.addEventListener('click', () => {
 close.addEventListener('click', () => {
     nav.classList.remove('right-0')
 })
+
+const lightbox = document.querySelector('.lightbox')
+const lightboxBody = document.querySelector('.lightbox-body')
+const images = document.querySelectorAll('#gallery')
+
+images.forEach((image) => {
+    image.addEventListener('click', () => {
+        lightbox.classList.add('scale-100')
+        const img = document.createElement('img')
+        img.classList.add('w-full')
+        img.classList.add('h-full')
+        img.classList.add('image-cover')
+        img.src = image.src
+        if (lightboxBody.childElementCount > 0) {
+            lightboxBody.removeChild(lightboxBody.children[0])
+        }
+        lightboxBody.appendChild(img)
+    })
+})
+
+lightbox.addEventListener('click', (event) => {
+    const idName = event.target.getAttribute('id')
+    if (idName === 'lightbox') {
+        lightbox.classList.remove('scale-100')
+    }
+})
+
+const year = document.querySelector('.year')
+const getYear = new Date().getFullYear()
+year.innerHTML = getYear
