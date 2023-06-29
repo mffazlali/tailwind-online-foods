2072e862839173a127c30240c21e565e4fdf535d
mohammad fazlali <58655751+mffazlali@users.noreply.github.com>
2023-06-29 10:10:36 -0700

    137-Make Project Ready for Deployment-git.ir.mp4

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js}'],
+  content: ['index.html','./src/**/*.{html,js}'],
   presets: [],
   darkMode: 'media', // or 'class'
   theme: {
